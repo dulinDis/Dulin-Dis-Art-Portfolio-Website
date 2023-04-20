82c1b10dd278e4a66dcbbafbb3b4dfe95dee771d
POkulska <paulina.okulska@gmail.com>
2023-04-20 16:24:39 +0200

    adjusted path for build in index.js

--- server/index.js
+++ server/index.js
@@ -43,43 +43,11 @@ app.use(bodyParser.json());
 app.use("/api", artworkRoutes.routes);
 app.use("/api", contactRoutes.routes);
 
-// if (
-//   process.env.NODE_ENV === `production` ||
-//   process.env.NODE_ENV === `staging`
-// ) {
-//   // app.use(express.static(path.join(__dirname, "..", "/client/build")));
-//   app.use(express.static(path.join(__dirname, "..", "client", "build")));
-//   // app.get(`*`, (req, res) => {
-//   //   res.sendFile(path.join(__dirname + "..", `/client/build/index.html`));
-//   // });
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
-//   });
-// }
-
 if (process.env.NODE_ENV === "production") {
-  app.use(
-    express.static(path.join(__dirname, "build"), {
-      setHeaders: function (res, path) {
-        if (mime.getType(path) === "text/html") {
-          // Set MIME type for HTML files
-          res.setHeader("Content-Type", "text/html");
-        } else if (mime.getType(path) === "application/javascript") {
-          // Set MIME type for JS files
-          res.setHeader("Content-Type", "application/javascript");
-        } else if (mime.getType(path) === "text/css") {
-          // Set MIME type for CSS files
-          res.setHeader("Content-Type", "text/css");
-        }
-      },
-    })
-  );
+  app.use(express.static(path.join(__dirname, "build")));
   app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "build", "index.html"));
   }); //for client side rendering
 }
 
 app.listen(PORT, () => console.log(`App is listening on port: ${PORT}`));
-
-// console.log(path.join(__dirname, "build"));
-// console.log(path.join(__dirname, "build", "index.html"));
